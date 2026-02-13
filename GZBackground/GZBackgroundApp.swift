a1af845c96231655e8d071647e71ed95974f9bc4
caiu <lacaiu69@gmail.com>
2026-02-13 14:46:45 -0300

    Initial Commit

---
+++ GZBackground/GZBackgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  GZBackgroundApp.swift
+//  GZBackground
+//
+//  Created by Caiu on 13/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct GZBackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
